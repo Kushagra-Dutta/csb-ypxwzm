c942fddbd644fac8052339a6e62a2d676eaa7577
Code Sandbox <hello@codesandbox.io>
2023-09-28 19:46:48 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8f5177bf6f4c3eb2fe5a5b7284ab466e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35c9b643dfaa756e09c62192fda0ebe",
+    "url": "https://kushagra-dutta.github.io/csb-ypxwzm/index.html"
+  },
+  {
+    "revision": "2b621e135df34e2aa394",
+    "url": "https://kushagra-dutta.github.io/csb-ypxwzm/static/js/2.46c8e90e.chunk.js"
+  },
+  {
+    "revision": "a95f68c0a67f62e4b6c7",
+    "url": "https://kushagra-dutta.github.io/csb-ypxwzm/static/js/main.de87ee4b.chunk.js"
+  },
+  {
+    "revision": "ff7ddb2b552a2ec6f1f0",
+    "url": "https://kushagra-dutta.github.io/csb-ypxwzm/static/js/runtime-main.afc7e0e6.js"
+  }
+]);
